198269b6175b94c90fc4fea27955f369acb19b1f
Rohit Kumar <115597404+RohitKumar2-2@users.noreply.github.com>
2023-12-16 19:11:47 +0530

    added files

---
+++ simple-python-app/start_container1.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -e
+
+# Pull the Docker image from Docker Hub
+docker pull docker pull rohitkumar170622/simple-python-flask-app
+
+# Run the Docker image as a container
+docker run -d -p 5000:5000  rohitkumar170622/simple-python-flask-app
