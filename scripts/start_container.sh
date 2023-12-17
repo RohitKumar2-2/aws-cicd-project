d235cc01fd03da72b64369fca1931d4b2f14120a
Rohit Kumar <115597404+RohitKumar2-2@users.noreply.github.com>
2023-12-17 10:22:50 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker pull docker pull rohitkumar170622/simple-python-flask-app
+docker pull rohitkumar170622/simple-python-flask-app
 
 # Run the Docker image as a container
 docker run -d -p 5000:5000  rohitkumar170622/simple-python-flask-app
